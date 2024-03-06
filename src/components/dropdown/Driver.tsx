442e0f865c91f30a6eb6c1b4930fbb08e986fe00
Eric Chu <Kiyuen88ec@gmail.com>
2024-03-05 18:39:23 -0800

    testing form with select

--- src/components/dropdown/Driver.tsx
+++ src/components/dropdown/Driver.tsx
@@ -1,33 +1,94 @@
-import { useState } from "react";
+import { FormEvent } from "react";
 import {
   Select,
   SelectContent,
+  SelectGroup,
   SelectItem,
+  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
 
-type Theme = "Light" | "Dark" | "System";
-const themes = ["Light", "Dark", "System"] as const;
-
 export function Driver() {
-  const [theme, setTheme] = useState<Theme>("Light");
+  function handleSubmission(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    console.log(formData.get("timezone"));
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-y-4">
-      <h1>{theme}</h1>
-      <Select onValueChange={(e) => setTheme(e as Theme)}>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Theme" />
-        </SelectTrigger>
-        <SelectContent>
-          {themes.map((n) => (
-            <SelectItem value={n} key={n}>
-              {n}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
+    <main className="flex min-h-screen items-center justify-center gap-y-4">
+      <form
+        onSubmit={handleSubmission}
+        className="flex flex-col items-center gap-y-4"
+      >
+        <Select required name="timezone">
+          <SelectTrigger className="w-[280px]">
+            <SelectValue placeholder="Select a timezone" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>North America</SelectLabel>
+              <SelectItem value="est">Eastern Standard Time (EST)</SelectItem>
+              <SelectItem value="cst">Central Standard Time (CST)</SelectItem>
+              <SelectItem value="mst">Mountain Standard Time (MST)</SelectItem>
+              <SelectItem value="pst">Pacific Standard Time (PST)</SelectItem>
+              <SelectItem value="akst">Alaska Standard Time (AKST)</SelectItem>
+              <SelectItem value="hst">Hawaii Standard Time (HST)</SelectItem>
+            </SelectGroup>
+            <SelectGroup>
+              <SelectLabel>Europe & Africa</SelectLabel>
+              <SelectItem value="gmt">Greenwich Mean Time (GMT)</SelectItem>
+              <SelectItem value="cet">Central European Time (CET)</SelectItem>
+              <SelectItem value="eet">Eastern European Time (EET)</SelectItem>
+              <SelectItem value="west">
+                Western European Summer Time (WEST)
+              </SelectItem>
+              <SelectItem value="cat">Central Africa Time (CAT)</SelectItem>
+              <SelectItem value="eat">East Africa Time (EAT)</SelectItem>
+            </SelectGroup>
+            <SelectGroup>
+              <SelectLabel>Asia</SelectLabel>
+              <SelectItem value="msk">Moscow Time (MSK)</SelectItem>
+              <SelectItem value="ist">India Standard Time (IST)</SelectItem>
+              <SelectItem value="cst_china">
+                China Standard Time (CST)
+              </SelectItem>
+              <SelectItem value="jst">Japan Standard Time (JST)</SelectItem>
+              <SelectItem value="kst">Korea Standard Time (KST)</SelectItem>
+              <SelectItem value="ist_indonesia">
+                Indonesia Central Standard Time (WITA)
+              </SelectItem>
+            </SelectGroup>
+            <SelectGroup>
+              <SelectLabel>Australia & Pacific</SelectLabel>
+              <SelectItem value="awst">
+                Australian Western Standard Time (AWST)
+              </SelectItem>
+              <SelectItem value="acst">
+                Australian Central Standard Time (ACST)
+              </SelectItem>
+              <SelectItem value="aest">
+                Australian Eastern Standard Time (AEST)
+              </SelectItem>
+              <SelectItem value="nzst">
+                New Zealand Standard Time (NZST)
+              </SelectItem>
+              <SelectItem value="fjt">Fiji Time (FJT)</SelectItem>
+            </SelectGroup>
+            <SelectGroup>
+              <SelectLabel>South America</SelectLabel>
+              <SelectItem value="art">Argentina Time (ART)</SelectItem>
+              <SelectItem value="bot">Bolivia Time (BOT)</SelectItem>
+              <SelectItem value="brt">Brasilia Time (BRT)</SelectItem>
+              <SelectItem value="clt">Chile Standard Time (CLT)</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <button className="w-full rounded-lg bg-neutral-950 p-2 text-white hover:bg-neutral-950/90">
+          Submit
+        </button>
+      </form>
     </main>
   );
 }
