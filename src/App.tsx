37cc1ccd002206c7c11e4e256204571907d542df
Eric Chu <Kiyuen88ec@gmail.com>
2024-03-25 16:57:50 -0700

    Initial creation of chess

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import { Route, Routes } from "react-router-dom";
-import { CentralNavBar } from "./components/CentralNavBar";
-import { Welcome } from "./components/Welcome";
-import { links } from "./lib/links";
-import { Driver } from "./components/wordle/Driver";
+import { useState } from "react";
+import { Position, Board, ChessPiece } from "./Moves";
 
-const debug = true;
-
-function App() {
-  if (debug) {
-    return <Driver />;
+function generateBoard(): Board {
+  const board: Board = {
+    cells: [],
+    pieces: [],
+  };
+  for (let x = 0; x < 8; x++) {
+    board.cells.push([]);
+    for (let y = 0; y < 8; y++) {
+      board.cells[x].push({
+        x,
+        y,
+        color: (x + y) % 2 ? "white" : "black",
+      });
+    }
   }
 
+  for (let x = 0; x < 8; x++) {
+    // Setting up black pawns on second row
+    board.pieces.push({
+      type: "pawn",
+      color: "black",
+      x: 1,
+      y: x,
+    });
+    board.pieces.push({
+      type: "pawn",
+      color: "white",
+      x: 6,
+      y: x,
+    });
+  }
+
+  return board;
+}
+
+function App() {
+  const [board, setBoard] = useState(generateBoard());
+  const [selected, setSelected] = useState<Position>();
+
+  function handleCellClick(piece: ChessPiece) {}
+
   return (
-    <Routes>
-      <Route path="/" element={<CentralNavBar />}>
-        <Route index element={<Welcome />} />
-        {links.map((n) => (
-          <Route key={n.path} path={n.path} element={n.element()} />
-        ))}
-      </Route>
-    </Routes>
+    <main className="grid min-h-screen grid-cols-8 bg-neutral-900 p-4">
+      {board.cells.map((row) =>
+        row.map((col) => {
+          const piece = board.pieces.find(
+            (p) => p.x === col.x && p.y === col.y,
+          );
+          return <Cell color={col.color} piece={piece} />;
+        }),
+      )}
+    </main>
   );
 }
 
 export default App;
+
+function Cell({
+  color,
+  piece,
+}: {
+  color: "white" | "black";
+  piece?: ChessPiece;
+}) {
+  return (
+    <div
+      className={`flex size-32 cursor-pointer items-center justify-center border ${color === "white" ? "bg-white" : "bg-black"}`}
+    >
+      <h1
+        className={`text-sm font-semibold ${piece?.color === "white" ? "text-red-400" : "text-blue-400"}`}
+      >
+        {piece ? piece.type : " "}
+      </h1>
+    </div>
+  );
+}
