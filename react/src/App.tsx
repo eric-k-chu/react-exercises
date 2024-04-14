313fc7193a9a742538b13ba5e726f57c95f5d17b
eric-k-chu <kiyuen88ec@gmail.com>
2024-04-14 14:55:00 -0700

    testing timer

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,7 +1,43 @@
-import { Driver } from "./components/chess/Driver";
+import { useState } from "react";
+// import { Driver } from "./components/chess/Driver";
 
 function App() {
-  return <Driver />;
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState<NodeJS.Timeout>();
+
+  function start(): void {
+    const timeId = setInterval(() => setTime((prev) => prev + 1), 1000);
+    setTimer(timeId);
+  }
+
+  function stop(): void {
+    clearInterval(timer);
+    setTime(0);
+    setTimer(undefined);
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-8">
+      <h1>{time}</h1>
+      {timer ? (
+        <button
+          type="button"
+          className="rounded-md border px-2 py-1"
+          onClick={stop}
+        >
+          Stop
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="rounded-md border px-2 py-1"
+          onClick={start}
+        >
+          Start
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default App;
